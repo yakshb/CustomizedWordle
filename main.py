5a572b6b64ce214fe171bd27344d896733708016
yakshb <60090011+yakshb@users.noreply.github.com>
2023-02-22 04:48:15 +0000

    Updated word library -- using nltk (natural language tool kit) word library vs. local txt file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import random
 import sys
 from termcolor import colored
-## import nltk 
-##nltk.download('words')
-##from nltk.corpus import words.txt
+import nltk 
+nltk.download('words')
+from nltk.corpus import words
 
 def print_menu():
-  print("Let's play Wordle:")
+  print("\nLet's play Wordle:")
   print("Type a 5 letter word and hit enter!\n")
 
 def read_random_word():
@@ -14,17 +14,17 @@ def read_random_word():
     words = f.read().splitlines()
     return random.choice(words)
 
-## nltk.data.path.append('/work/words')
-## word_list = words.words()
-## words_five = [word for word in word_list if len(word == 5)]
+nltk.data.path.append('/work/words')
+word_list = words.words()
+words_five = [word for word in word_list if len(word) == 5]
 
-print_menu()
 play_again = ""
 while play_again != "q":
+  print_menu()
   ## Second number is exclusive in range()
   
-  word = read_random_word()
-  # word = random.choice(words_five)
+  # word = read_random_word()
+  word = random.choice(words_five)
   for attempt in range(1, 7):
     guess = input().lower()
   
@@ -45,7 +45,7 @@ while play_again != "q":
       print(colored(f"\nCongrats! You got the wordle in {attempt}", 'blue'))
       break
     elif attempt == 6:
-      print(f"Boohoo! Today's wordle is.. {word}")
+      print(colored(f"Boohoo! Today's wordle is.. {word}", 'red'))
 
     
   play_again = input("Play Again? Type Q to Exit")
